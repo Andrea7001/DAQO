41956005d12b1a49e17a740eb150e69dbc484291
Andrea7001 <qo24001@esfe.agape.edu.sv>
2024-10-10 13:27:11 -0600

    cambios

--- DAQO.StringManipulator.UnitTestXUnit/StringManipulatorTests.cs
+++ DAQO.StringManipulator.UnitTestXUnit/StringManipulatorTests.cs
@@ -13,26 +13,25 @@ public class StringManipulatorTests
     [Fact]
     public void ReverseString_InputIsHello_ShouldReturnOlleh()
     {
-        // Arrange
+        
         var input = "Hola";
-
-        // Act
+ 
         var result = _stringManipulator.ReverseString(input);
 
-        // Assert
+        
         Assert.Equal("aloH", result);
     }
 
     [Fact]
     public void RemoveSpaces_InputHasSpaces_ShouldRemoveAllSpaces()
     {
-        // Arrange
+        
         var input = "H o l a";
 
-        // Act
+        
         var result = _stringManipulator.RemoveSpaces(input);
 
-        // Assert
+         
         Assert.Equal("Hola", result);
     }
 }
